9faef2d2e78816a19836911792271e8d4ac698da
rowanamgad <76775219+rowanamgad@users.noreply.github.com>
2022-09-07 12:27:20 +0200

    registration api integrated and working

--- app/src/main/java/com/evapharma/animalhealth/authflow/presentation/ui/fragments/OtpFragment.kt
+++ app/src/main/java/com/evapharma/animalhealth/authflow/presentation/ui/fragments/OtpFragment.kt
@@ -1,23 +1,73 @@
 package com.evapharma.animalhealth.authflow.presentation.ui.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.LifecycleCoroutineScope
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.work.impl.Schedulers.schedule
 import com.evapharma.animalhealth.R
 import com.evapharma.animalhealth.authflow.domain.model.RegResponseModel
+import com.evapharma.animalhealth.databinding.FragmentOtpBinding
+import com.evapharma.animalhealth.databinding.RegistrationCustomDialogBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.*
+import kotlin.concurrent.schedule
 
+@AndroidEntryPoint
 class OtpFragment : Fragment() {
 
+    private lateinit var binding : FragmentOtpBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-
-
+        binding = FragmentOtpBinding.inflate(layoutInflater)
         val response = arguments?.getParcelable<RegResponseModel>("response")
-
         Log.d("MyApp", "flag : ${response?.isSuccess} , body: ${response?.message}")
+//        val successFlag:Boolean = (response!!).isSuccess
+        binding.VerifyBTN.setOnClickListener {
+//            Log.d("first", binding.OTPfield1.editText.toString())
+            val customDialog = RegistrationCustomDialogBinding.inflate(layoutInflater)
+            val customAlertDialog = AlertDialog.Builder(this.context).setView(customDialog.root).create()
+            if(response?.isSuccess == true){
+                Log.d("here", "henaa")
+                customDialog.registrationMsg.setText("Registration Successful!")
+                customAlertDialog.show()
+//                lifecycleScope.launch {
+//                    delay(2000)
+//                    findNavController().navigate(R.id.action_otpFragment_to_loginFragment)
+//                    customAlertDialog.dismiss()
+//                }
+                Timer().schedule(2000) {
+//                    println("1 second elapsed")
+                    findNavController().navigate(R.id.action_otpFragment_to_loginFragment)
+                    customAlertDialog.dismiss()
+                }
+            }
+            else{
+                customDialog.registrationMsg.setText("Something went wrong. Please register again")
+                customAlertDialog.show()
+//                lifecycleScope.launch {
+//                    delay(2000)
+//                    findNavController().navigate(R.id.action_otpFragment_to_loginFragment)
+//                    customAlertDialog.dismiss()
+//                }
+                Timer().schedule(2000) {
+                    findNavController().navigate(R.id.action_otpFragment_to_loginFragment)
+                    customAlertDialog.dismiss()
+                }
+
+                }
+
+        }
+
+
 
         return inflater.inflate(R.layout.fragment_otp, container, false)
     }
--- app/src/main/java/com/evapharma/animalhealth/authflow/presentation/ui/fragments/RegisterFragment.kt
+++ app/src/main/java/com/evapharma/animalhealth/authflow/presentation/ui/fragments/RegisterFragment.kt
@@ -2,6 +2,7 @@ package com.evapharma.animalhealth.authflow.presentation.ui.fragments
 
 import android.os.Bundle
 import android.text.TextUtils.replace
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -33,15 +34,19 @@ class RegisterFragment : Fragment() {
         registerViewModel = ViewModelProvider(this)[AuthViewModel::class.java]
 
         binding.RegisterBtn.setOnClickListener {
-            val customer = CustomerModel(binding.nameInput.toString(), binding.mobileInput.toString(), binding.PasswordInput.toString())
+            val customer = CustomerModel(binding.nameInput.text.toString(), binding.mobileInput.text.toString(), binding.PasswordInputText.text.toString())
+            Log.d("Customer Model", "Name: ${customer.UserName} ,  password: ${customer.Password}, number: ${customer.PhoneNumber}")
             CoroutineScope(Dispatchers.IO).launch {
                 val response = registerViewModel.registerCustomer(customer)
                 withContext(Dispatchers.Main){
                     val bundle = Bundle()
                     bundle.putParcelable("response", response)
-                    val otpFragment = OtpFragment()
-                    otpFragment.arguments = bundle
-                    transferTo(otpFragment)
+//                    val otpFragment = OtpFragment()
+//                    otpFragment.arguments = bundle
+//                    transferTo(otpFragment)
+                    Log.d("responseFlag:", response.isSuccess.toString())
+                    Log.d("responseMessage", response.message)
+                    findNavController().navigate(R.id.action_registerFragment_to_otpFragment, bundle)
                 }
             }
         }
