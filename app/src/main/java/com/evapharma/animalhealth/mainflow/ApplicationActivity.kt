a540afe7aac9dd76b6e183a5a6e28f8838c4268e
bafely x <bafely20191700177@cis.asu.edu.eg>
2022-09-13 11:26:43 +0200

    made some adjustments to ui of auth flow

--- app/src/main/java/com/evapharma/animalhealth/mainflow/ApplicationActivity.kt
+++ app/src/main/java/com/evapharma/animalhealth/mainflow/ApplicationActivity.kt
@@ -1,10 +1,16 @@
 
 package com.evapharma.animalhealth.mainflow
 
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import com.evapharma.animalhealth.R
 import com.evapharma.animalhealth.authflow.presentation.ui.AuthActivity
@@ -54,4 +60,22 @@ class ApplicationActivity : AppCompatActivity() {
             replace(R.id.nav_container,fragment)
             commit()
         }
+
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val v: View? = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm: InputMethodManager =
+                        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
+    }
 }
+
